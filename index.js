a544ad5c2b37c65cf139af991447f4c565af4279
ThijmenGThN <thijmenheuvelink.gthn@protonmail.com>
2021-03-15 15:20:21 +0100

    Rewrite
    If you wish to collaborate or contact me, be sure to message me on Discord.
    
    ~@ThijmenGThN#2684

--- index.js
+++ index.js
@@ -1,62 +1,79 @@
-// Nodejs module inorder to interact with discord api
-const Discord = require('discord.js');
-const {prefix, BOT_TOKEN, channel_id} = require('./config.json');
-const commandOne = require('./command/execute');
-const commandTwo = require('./command/reply');
-const commandThree = require('./command/skip');
-const commandFour = require('./command/stop');
 
-// It interact with Discord api.
-const Bot = new Discord.Client();
+// Import environment config
+const {prefix, BOT_TOKEN, channel_id} = require('./config.json')
 
-// Queue for storing songs
-const queue = new Map();
+// Build global initiator
+const G = {
+    discord: new require(`discord.js`).Client(),
+    command: {
+        execute: require(`./command/execute`),
+        reply: require(`./command/reply`),
+        skip: require(`./command/skip`),
+        stop: require(`./command/stop`)
+    },
+    queue = new Map()
+}
+
 
 // client start working
-Bot.once('ready', () => {
-  console.log('Ready as ' + Bot.user.tag);
+G.discord.once('ready', () => {
 
-  // bot user's activity
-  Bot.user.setActivity('your Message', {type: 'WATCHING'});
+    console.log('Ready as ' + G.discord.user.tag)
 
-  // Bot may be connected to different server like guilds  console.log(bot.guilds);
-  // So u want the bot to welcome once it become online.
-  // There may be diff. channels on discord.
-  Bot.channels
-    .fetch(channel_id)
-    .then((channel) =>
-      channel.send(`Hello my gorgeous friend on the internet!`)
-    )
-    .catch(console.error);
+    // G.discord user's activity
+    G.discord.user.setActivity('your Message', {type: 'WATCHING'})
+
+        // G.discord may be connected to different server like guilds  console.log(G.discord.guilds);
+        // So u want the G.discord to welcome once it become online.
+        // There may be diff. channels on discord.
+        G.discord.channels
+            .fetch(channel_id)
+            .then((channel) =>
+                channel.send(`Hello my gorgeous friend on the internet!`)
+            )
+            .catch(console.error)
+})
+
+
+// G.discord listening to the message.
+G.discord.on('message', (message) => {
+
+    // Content filter
+    if (message.author.G.discord) return
+    if (!message.content.startsWith(prefix)) return
+
+    const serverStack = queue.get(message.guild.id)
+
+    // like !ping => ! is prefix and ping is command name
+    const commandBody = message.content.slice(prefix.length)
+    const command = commandBody.toLowerCase()
+
+    // Editing the music commands
+    const args = message.content.split(' ')
+    const musicCom = args[0].slice(prefix.length)
+    const com = musicCom.toLocaleLowerCase()
+
+    // TimeStamp
+    const timeTaken = Date.now() - message.createdTimestamp
+
+    // Command handler
+    switch (command) {
+        case `ping`: return message.reply(`Hey, This message had a latency of ${timeTaken}ms.`)
+        case `my avatar?`: return message.reply(message.author.displayAvatarURL())
+        case `hi`: return commandTwo.reply(message)
+    }
+
+    // Comm handler
+    switch (com) {
+        case `play`: return commandOne.execute(message, args, serverStack, queue)
+        case `skip`: return commandThree.skip(message, serverStack)
+        case `stop`: return commandFour.stop(message, serverStack)
+    }
+
+    // Return unknown command error
+    if (command || com) return message.reply('Bruh that command is not in my fort!!')
+
 });
 
-// bot listening to the message.
-Bot.on('message', (message) => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  const serverStack = queue.get(message.guild.id);
-
-  // like !ping => ! is prefix and ping is command name
-  const commandBody = message.content.slice(prefix.length);
-  const command = commandBody.toLowerCase();
-
-  // Editing the music commands
-  const args = message.content.split(' ');
-  const musicCom = args[0].slice(prefix.length);
-  const com = musicCom.toLocaleLowerCase();
-
-  // TimeStamp
-  const timeTaken = Date.now() - message.createdTimestamp;
-
-  if (command === 'ping') return message.reply(`Hey, This message had a latency of ${timeTaken}ms.`);
-  if (command === 'my avatar?') return message.reply(message.author.displayAvatarURL());
-  if (com === 'play') return commandOne.execute(message, args, serverStack, queue);
-  if (command === 'hi') return commandTwo.reply(message);
-  if (com === 'skip') return commandThree.skip(message, serverStack);
-  if (com === 'stop') return commandFour.stop(message, serverStack);
-  if (command || com) return message.reply('Bruh that command is not in my fort!!');
-});
-
-// Connecting to the bot using tokens
-Bot.login(BOT_TOKEN);
+// Connecting to the G.discord using tokens
+G.discord.login(BOT_TOKEN)
